e2dcb4f25525d0586a72b5f7792fa960af9150bc
sajid43677 <20-43677-2@student.aiub.edu>
2022-10-16 16:13:02 +0600

    LABTASK4

--- LAB TASK 4/loginCheck.php
+++ LAB TASK 4/loginCheck.php
@@ -1,16 +1,24 @@
 <?php
     session_start();
     $spchar = 0;
-    if(isset($_POST["Id"])){
+    if(isset($_POST["Id"]) && isset($_POST["Password"])){
         $Password = $_POST["Password"];
+        $Name = $_POST["Id"];
         $sz = strlen($Password);
         for($i = 0; $i < $sz;$i++){
             if($Password[$i] == '@'|| $Password[$i] == '$' || $Password[$i] == '%' || $Password[$i] == '#'){
                 $spchar++;
             }
         }
+        $sz = strlen($Name);
+        $spchar1 = 0;
+        for($i = 0; $i < $sz;$i++){
+            if($Name[$i] != '.'&& $Name[$i] != '_' && $Name[$i] != '-' && !ctype_alnum($Name[$i])){
+                $spchar1++;
+            }
+        }
     }
-    if(strlen($_POST["Id"]) < 2 || !ctype_alnum($_POST["Id"]) || strlen($_POST["Password"]) < 8 || $spchar < 1){
+    if(strlen($_POST["Id"]) < 2 || $spchar1 > 0 || strlen($_POST["Password"]) < 8 || $spchar < 1){
         $_SESSION["validate"] = "INVALID USERNAME or PASSWORD!";
         header("location:login.php");
     }
