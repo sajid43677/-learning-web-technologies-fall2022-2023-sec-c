053563b3e8df83e6c8c0568f40bfe1ccf62aa252
sajid43677 <20-43677-2@student.aiub.edu>
2022-10-16 16:53:58 +0600

    Labtask4

---
+++ LAB TASK 4/registration.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+	<title>FORM</title>
+</head>
+<body>
+	<?php 
+		session_start();
+		if(isset($_SESSION["validate"])){
+			echo $_SESSION["validate"];
+			session_destroy();
+		}
+	?>
+		<form method="post" action="loginCheck.php" enctype="">
+			<fieldset>
+				<legend><b>Registration</b></legend>
+				<table>
+					<tr>
+						<td>Name</td>
+						<td><input type="text" name="Name" value="" ></td>
+					</tr>
+                    <tr>
+                        <td colspan="2"><hr></td>
+                    </tr>
+					<tr>
+						<td>Email</td>
+						<td><input type="text" name="Email" value="" ></td>
+					</tr>
+                    <tr>
+                        <td colspan="2"><hr></td>
+                    </tr>
+                    <tr>
+						<td>User Name</td>
+						<td><input type="text" name="Name" value="" ></td>
+					</tr>
+                    <tr>
+                        <td colspan="2"><hr></td>
+                    </tr>
+					<tr>
+						<td>Password</td>
+						<td><input type="password" name="Password" value="" ></td>
+					</tr>
+                    <tr>
+                        <td colspan="2"><hr></td>
+                    </tr>
+                    <tr>
+						<td>Confirm Password</td>
+						<td><input type="password" name="ConPassword" value="" ></td>
+					</tr>
+                    <tr>
+                        <td colspan="2"><hr></td>
+                    </tr>
+					<tr>
+						<td colspan="2">
+							<input type="submit" name="" value="Submit">
+							<input type="reset" name="" value="Reset">
+						</td>
+					</tr>
+				</table>
+			</fieldset>
+		</form>
+
+</body>
+</html>
