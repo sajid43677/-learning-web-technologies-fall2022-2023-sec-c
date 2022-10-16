b20dd82ed0adff19a78e00fa44265542c405c6f7
sajid43677 <20-43677-2@student.aiub.edu>
2022-10-16 15:49:28 +0600

    labtask4

--- LAB TASK 4/loginCheck.php
+++ LAB TASK 4/loginCheck.php
@@ -1,6 +1,16 @@
 <?php
-session_start();
-    if(strlen($_POST["Id"]) < 2 || !ctype_alnum($_POST["Id"])){
+    session_start();
+    $spchar = 0;
+    if(isset($_POST["Id"])){
+        $Password = $_POST["Password"];
+        $sz = strlen($Password);
+        for($i = 0; $i < $sz;$i++){
+            if($Password[$i] == '@'|| $Password[$i] == '$' || $Password[$i] == '%' || $Password[$i] == '#'){
+                $spchar++;
+            }
+        }
+    }
+    if(strlen($_POST["Id"]) < 2 || !ctype_alnum($_POST["Id"]) || strlen($_POST["Password"]) < 8 || $spchar < 1){
         $_SESSION["validate"] = "INVALID USERNAME or PASSWORD!";
         header("location:login.php");
     }
